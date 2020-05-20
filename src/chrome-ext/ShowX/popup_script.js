1a2c54c63839fce0a603bcc8809f0cae577332a7
dudung <dudung@gmail.com>
2020-05-20 09:32:12 +0700

    add icons

--- src/chrome-ext/ShowX/popup_script.js
+++ src/chrome-ext/ShowX/popup_script.js
@@ -1,3 +1,7 @@
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
 document.addEventListener("DOMContentLoaded", function(dcle) {
 	var p = document.createElement("p");
 	document.body.append(p);
