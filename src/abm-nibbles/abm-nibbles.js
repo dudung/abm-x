844e93573709a2543ea0a099487d0b9b0b400d4c
dudung <dudung@gmail.com>
2020-05-22 07:26:44 +0700

    con moving worm

--- src/abm-nibbles/abm-nibbles.js
+++ src/abm-nibbles/abm-nibbles.js
@@ -9,6 +9,9 @@
 	0611 Try to run abm-in-rect.js with abm-base.js library.
 	0614 Clean and distribute [2] to abm-base.js, abm-nibbles.js.
 	0642 Create a linear worm with direction.
+	0711 Move worm in horizontal or vertical direction in a step.
+	0717 Set random direction, worm may be trapped with its tail.
+	0726 Add event for pause the simulation.
 	
 	References
 	1. url https://en.wikipedia.org/w/index.php?oldid=945651481
@@ -43,21 +46,23 @@ function main() {
 	addBorder(world);
 	
 	// Create a worm
-	var N = 5;
+	var N = 10;
 	var mode = "linier-vertical-to-bottom";
-	worm = createWorm(N, xmin + 6, ymin + 6, mode);
+	var cBody = 2;
+	var cHead = 0;
+	worm = createWorm(N, xmin + 6, ymin + 2, mode, cBody, cHead);
 	
 	addAgent(worm).toWorld(world);
 	viewWorld(world, worm).inElement(p);
 	
 	console.log(pname);
 	
-	/*
-	var proc = setInterval(simulate, 100);
+	var PAUSE = false;
+	var proc = setInterval(simulate, 20);
 	
 	var iter = 0
-	var maxIter = 200;
-	var startIter = 40;
+	var maxIter = 500;
+	var startIter = 20;
 	
 	function simulate() {
 		if(iter > startIter) {
@@ -72,8 +77,60 @@ function main() {
 			clearInterval(proc);
 		}
 	}
-	*/
+	
+	document.body.addEventListener("click", function() {
+		if(!PAUSE) {
+			PAUSE = true;
+			clearInterval(proc);
+		} else {
+			PAUSE = false;
+			proc = setInterval(simulate, 20);
+		}
+	});
 }
+
+
+// Move worm as connected agents
+function moveWorm() {
+	var agents = arguments[0];
+	var world = arguments[1];
+	
+	var N = agents.length;
+	
+	var di, dj;
+	var dir = Math.round(Math.random() * 20);
+	if(dir <= 9) { di = 1; dj = 0; }
+	if(dir == 10) { di = 0; dj = 1; }
+	if(dir == 11) { di = 0; dj = -1; }
+	if(dir >= 12) { di = -1; dj = 0; }
+	
+	
+	var k = 0;
+	var i = agents[k].x;
+	var j = agents[k].y;
+	
+	// Move src to dest if empty
+	if(world[j + dj][i + di] == -1) {
+		world[j + dj][i + di] = k;
+		world[j][i] = -1;
+		agents[k].x = i + di;
+		agents[k].y = j + dj;
+		
+		for(var k = 1; k < N; k++) {
+			var i2 = agents[k].x;
+			var j2 = agents[k].y;
+			
+			world[j][i] = k;
+			world[j2][i2] = -1;	
+			agents[k].x = i;
+			agents[k].y = j;
+
+			i = i2;
+			j = j2;
+		}	
+	}
+}
+
 
 
 // Create a worm
@@ -82,17 +139,17 @@ function createWorm() {
 	var xo = arguments[1];
 	var yo = arguments[2];
 	var mode = arguments[3];
+	var color1 = arguments[4];
 	
 	var worm = [];
 	if(mode == "linier-horizontal-to-right") {
 		for(var i = 0; i < N; i++) {
 			var xx = xo + i;
 			var yy = yo;
-			var cc = 0;
 			var agent = {
 				x: xx,
 				y: yy,
-				c: cc,
+				c: color1,
 			};
 			worm.push(agent);
 		}
@@ -101,11 +158,10 @@ function createWorm() {
 		for(var i = 0; i < N; i++) {
 			var xx = xo - i;
 			var yy = yo;
-			var cc = 0;
 			var agent = {
 				x: xx,
 				y: yy,
-				c: cc,
+				c: color1,
 			};
 			worm.push(agent);
 		}
@@ -114,11 +170,10 @@ function createWorm() {
 		for(var i = 0; i < N; i++) {
 			var xx = xo;
 			var yy = yo + i;
-			var cc = 0;
 			var agent = {
 				x: xx,
 				y: yy,
-				c: cc,
+				c: color1,
 			};
 			worm.push(agent);
 		}
@@ -127,14 +182,19 @@ function createWorm() {
 		for(var i = 0; i < N; i++) {
 			var xx = xo;
 			var yy = yo - i;
-			var cc = 0;
 			var agent = {
 				x: xx,
 				y: yy,
-				c: cc,
+				c: color1,
 			};
 			worm.push(agent);
 		}
+	}
+	
+	// Color the head if provided
+	var color2 = arguments[5];
+	if(color2 != undefined) {
+		worm[0].c = color2;
 	}
 	
 	return worm;
