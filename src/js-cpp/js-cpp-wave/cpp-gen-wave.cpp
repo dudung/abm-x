37c24bbb2d8cb58bbbf0f460f5df1d97616e7a5e
dudung <dudung@gmail.com>
2020-05-30 04:37:17 +0700

    doc js-cpp-wave

--- src/js-cpp/js-cpp-wave/cpp-gen-wave.cpp
+++ src/js-cpp/js-cpp-wave/cpp-gen-wave.cpp
@@ -11,6 +11,13 @@
 	2007 Derive it from randint.cpp file.
 	2047 Clear the streamstring [1].
 	2116 Finish producing wave profile.
+	2123 Add time stamp in the beginning of data.
+	2146 Change format to JSON manually.
+	2151 Corret JSON output, remove last semicolon.
+	2218 Change delay to 0.1 s and test it.
+	2221 Find problem with the last comma in array.
+	2239 Fix range dependent wavelength.
+	2251 Problem not until final time tend.
 	
 	References
 	1. https://stackoverflow.com/a/2848109/9475509
@@ -29,10 +36,12 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	// Prepare stringstream
 	stringstream sout;
+	stringstream xout;
+	stringstream yout;
 	
 	// Define wave parameters
 	double A = 0.1;       // m
-	double T = 1.0;       // s
+	double T = 0.5;       // s
 	double lambda = 1.0;  // main
 	double phi0 = 0;      // rad
 	
@@ -42,10 +51,11 @@ int main(int argc, char *argv[]) {
 	
 	// Define ranges
 	double xmin = 0;
-	double xmax = xmin + 2 * lambda;
+	double xmax = xmin + 6 * lambda;
 	double dx = 0.01 * lambda;
+	int Nx = round((xmax - xmin) / dx);
 	double tmin = 0;
-	double tmax = tmin + 10 * T;
+	double tmax = tmin + 20 * T;
 	double dt = 0.1 * T;
 	
 	// Initiate time
@@ -55,27 +65,53 @@ int main(int argc, char *argv[]) {
 		// Clear stringstream
 		sout.str(string());
 		sout.clear();
+		xout.str(string());
+		xout.clear();
+		yout.str(string());
+		yout.clear();
 		
-		// Add prefix data
-		sout << "var data =`" << endl;
+		// Begin JSON format
+		sout << "var data =`{" << endl;
+		
+		// Add time information
+		sout << "\"t\": " << t << "," << endl;
+		
+		// Begin array
+		xout << "\"x\": [";
+		yout << "\"y\": [";
 		
 		// Initiate position
 		double x = xmin;
-		while(x <= xmax + 0.01 * dx) {
+		int ix = 0;
+		while(ix <= Nx) {
+			x = xmin + ix * dx;
 			
 			// Calculate wave data
 			double y = A * sin(k * x - omega * t + phi0);
 			
 			// Add content data
-			sout << x << "\t";
-			sout << y << endl;
+			xout << x;
+			yout << y;
+			
+			if(ix < Nx) {
+				xout << ", ";
+				yout << ", ";
+			}
 			
 			// Advance position
-			x += dx;
+			ix++;
 		}
 		
-		// Add sufix data
-		sout << "`" << endl;
+		// End array
+		xout << "]";
+		yout << "]";
+		
+		// Add array
+		sout << xout.str() << "," << endl;
+		sout << yout.str() << endl;
+		
+		// End JSON format
+		sout << "}`" << endl;
 		
 		// Write data to file
 		ofstream fout;
@@ -88,7 +124,7 @@ int main(int argc, char *argv[]) {
 		cout.flush();
 		
 		// Delay
-		this_thread::sleep_for(0.5s);
+		this_thread::sleep_for(0.1s);
 		
 		// Advance time
 		t += dt;
--- src/js-cpp/js-cpp-wave/js-show-wave.js
+++ src/js-cpp/js-cpp-wave/js-show-wave.js
@@ -5,7 +5,16 @@
 	Sparisoma Virid | https://github.com/abm-x
 	
 	20200529
-	2005 Derive it from js-cpp-experment.js file.	
+	2005 Derive it from js-cpp-experment.js file.
+	2146 View first string of JSON from data.txt file.
+	2232 Get error Uncaught SyntaxError: Unexpected end of input.
+	2235 Correct xrange from waveCurve.
+	2244 Can avoid error message using [1], but not understand.
+	2246 The error comes back.
+	
+	References
+	1. url https://www.w3schools.com/js/js_errors.asp
+	  [20200529].
 */
 
 
@@ -20,36 +29,52 @@ main();
 // Define main function
 function main() {
 	iter = 0;
-	iterMax = 1000;
+	iterMax = 10000;
 	
 	var p = document.createElement("p");
-	p.id = "output";
-	p.style.fontWeight = "bold";
-	p.style.fontSize = "40px";
+	p.id = "outText";
 	document.body.append(p);
+	
+	var c = document.createElement("canvas");
+	c.id = "outCanvas";
+	c.width = "600";
+	c.height = "60";
+	c.style.width = c.width + "px";
+	c.style.height = c.height + "px";
+	c.style.background = "#fff";
+	c.style.border = "#fff 0px solid";
+	document.body.append(c);
 	
 	document.addEventListener("click", function() {
 		clearInterval(proc);
 		console.log("Click end the process");
 	});
 	
-	proc = setInterval(read, 1000);
+	proc = setInterval(read, 50);
 }
 
 
 // Read the data through a script
 function read() {
-	var q = document.getElementById("output");
+	var q = document.getElementById("outText");
+	var d = document.getElementById("outCanvas");
 	
 	var script = document.createElement("script");
 	script.type = "text/javascript";
 	script.src = "data.txt";
 	script.onload = function() {
-		var bb = ("0" + data.toString(16)).slice(-2);
-		var rr = 255 - data;
-		rr = ("0" + rr.toString(16)).slice(-2);
-		q.style.color = "#" + rr + "00" + bb; 
-		q.innerHTML = ("00" + data).slice(-3);
+		
+		try {
+			var waveCurve = JSON.parse(data);
+			clear(d);
+			draw(d, waveCurve);
+			
+			q.innerHTML = "t = " + waveCurve.t + " s";
+		} catch(err) {
+			console.log(err);
+			console.log(data);
+		}
+		
 	}
 	document.body.append(script);
 	
@@ -62,3 +87,68 @@ function read() {
 		console.log("Maximum iteration achieved");
 	}
 }
+
+
+// Draw wave on canvas
+function draw() {
+	var c = arguments[0];
+	var w = arguments[1];
+	var cx = c.getContext("2d");
+	
+	var x = w.x;
+	var y = w.y;
+	var N = x.length;
+	
+	cx.strokeStyle = "#f00";
+	cx.lineWidth = 2;
+	cx.beginPath();
+	for(var i = 0; i < N; i++) {
+		var xi = x[i];
+		var yi = y[i];
+		var TR = tr(xi, yi);
+		var X = TR[0];
+		var Y = TR[1];
+		
+		if(i == 0) {
+			cx.moveTo(X, Y);
+		} else {
+			cx.lineTo(X, Y);
+		}
+	}
+	cx.stroke();
+	
+	function tr() {
+		var XMIN = 1;
+		var XMAX = parseInt(c.width) - 1;
+		var YMIN = parseInt(c.height) - 1;
+		var YMAX = 1;
+			
+		var xmin = w.x[0];
+		var xmax = w.x[w.x.length - 1];
+		var ymin = -0.1;
+		var ymax = 0.1;
+		
+		var xx = arguments[0];
+		var yy = arguments[1];
+		
+		var XX = (xx - xmin) / (xmax - xmin);
+		XX *= (XMAX - XMIN);
+		XX += XMIN;
+		
+		var YY = (yy - ymin) / (ymax - ymin);
+		YY *= (YMAX - YMIN);
+		YY += YMIN;
+		
+		return [XX, YY];
+	}
+}
+
+// Clear canvas
+function clear() {
+	var c = arguments[0];
+	var LX = parseInt(c.width);
+	var LY = parseInt(c.height);
+	var cx = c.getContext("2d");
+	cx.clearRect(0, 0, LX, LY);
+}
+
