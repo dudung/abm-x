1324271af5ca24401b6540296392594c677b6c2d
dudung <dudung@gmail.com>
2020-06-01 21:35:56 +0700

    beg to experiment script-create-2

---
+++ src/experiment/js-script-create-2/README.md
@@ -0,0 +1,8 @@
+# js-script-create-2
+Explore HTML DOM element `script`
+
+## Note
+Try to resolve the failed of [js-script-create](../js-script-create)
+
+## Result
+
---
+++ src/experiment/js-script-create-2/js-script-create-2.js
@@ -0,0 +1,19 @@
+/*
+	js-script-create-2.js
+	Explore HTML DOM element script
+	
+	Sparisoma Virid | https://github.com/abm-x
+	
+	20200601
+	2133 Continue the failed js-script-create file.
+*/
+
+
+// Execute main function
+main();
+
+
+// Define main function
+function main() {
+}
+
