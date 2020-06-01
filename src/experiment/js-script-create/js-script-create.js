c983a95829ce6c8729348505b6a6221b4dee7bf3
dudung <dudung@gmail.com>
2020-06-01 17:46:20 +0700

    con to experiment

--- src/experiment/js-script-create/js-script-create.js
+++ src/experiment/js-script-create/js-script-create.js
@@ -6,8 +6,14 @@
 	
 	20200601
 	1655 Create this example.
+	1730 Try onload but not yet implement [1].
+	1736 Implement [2] for checking inexisting variables [2].
 	
 	References
+	1. url https://stackoverflow.com/a/27000218/9475509
+	   [20200601].
+	2. url https://stackoverflow.com/a/30003227/9475509
+	   [20200601].
 */
 
 
@@ -17,13 +23,45 @@ main();
 
 // Define main function
 function main() {
+	/*
 	// Create HTML DOM element
 	var script = document.createElement("script");
 	script.src = "data.js";
 	document.body.append(script);
 	
+	if(window.hasOwnProperty("data_js")) {
+		console.log(data_js);
+	}
 	
+	script.onload = function() {
+		var e = arguments[0];
+		
+		if(window.hasOwnProperty("data_js")) {
+			console.log(data_js);
+		}
+	}
+	*/
+}
+
+
+// Include script and get variables with name
+function isngvwn() {
+	var src = arguments[0];
+	var name = arguments[1];
 	
-	console.log(data_js);
+	// Create HTML DOM element
+	var script = document.createElement("script");
+	script.src = src;
+	document.body.append(script);
+	
+	var value;
+	
+	script.onload = function() {
+		if(window.hasOwnProperty(name)) {
+			value = window[name];
+		}
+	}
+	
+	return value;
 }
 
