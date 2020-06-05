1eada574b9af81c1942b909e60219b756fcec536
dudung <dudung@gmail.com>
2020-06-05 17:51:19 +0700

    create analyzer

--- src/app/abm-odm/abm-odm-data.js
+++ src/app/abm-odm/abm-odm-data.js
@@ -10,26 +10,86 @@
 */
 
 
-// Define global variabels
-var rawdata;
-
-
 // Call main function
 main();
 
 
+// Define main function
 function main() {
-	readAllRawData();
+	var rawdata = readAllRawData();
+	
+	var data1 = [];
+	for(var i = 0; i < rawdata.length; i++) {
+		var rd = removeEmptyLines(rawdata[i]);
+		data1.push(rd);
+	}
+	
+	var data2 = [];
+	for(var i = 0; i < data1.length; i++) {
+		var rd = keepLinesWithMark(data1[i], "|");
+		data2.push(rd);
+	}
+	
+	data3 = [];
+	for(var i = 0; i < data2.length; i++) {
+		var rd = toColumnData(data2[i], "|");
+		data3.push(rd);
+	}
+}
+
+
+// Convert to column data with separator mark
+function toColumnData() {
+	var rd = arguments[0];
+	var mark = arguments[1];
+	
+	var agent = [];
+	var city = [];
+	var iter = [];
+	
+	var lines = rd.split("\n");
+	for(var i = 0; i < lines.length; i++) {
+		var row = lines[i];
+		var cols = row.split(mark);
+		
+		agent.push(cols[0].trim());
+	}
+	
+	return [agent];
+}
+
+
+// Keep only lines with certain mark
+function keepLinesWithMark() {
+	var rd = arguments[0];
+	var mark = arguments[1];
+	lines = rd.split("\n");
+	var N = lines.length;
+	if(lines[0].indexOf(mark) < 0) lines.shift();
+	return lines.join("\n");	
+}
+
+
+// Remove EmptyLines
+function removeEmptyLines() {
+	var rd = arguments[0];
+	var lines = rd.split("\n");
+	var N = lines.length;
+	if(lines[N - 1].length == 0) lines.pop();
+	if(lines[0].length == 0) lines.shift();
+	return lines.join("\n");
 }
 
 
 // Read all raw data
 function readAllRawData() {
-	rawdata = [];
-	rawdata.push(data01);
-	rawdata.push(data02);
-	rawdata.push(data03);
-	rawdata.push(data04);
-	rawdata.push(data05);
+	var raw = [];
+	raw.push(data01);
+	raw.push(data02);
+	raw.push(data03);
+	raw.push(data04);
+	raw.push(data05);
+	raw.push(data06);
+	return raw;
 }
 
