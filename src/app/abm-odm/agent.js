2a7c77108c7fd54d8748761825e95d4d8d12702d
dudung <dudung@gmail.com>
2020-06-05 08:39:21 +0700

    create and test Agent

---
+++ src/app/abm-odm/agent.js
@@ -0,0 +1,78 @@
+/*
+	agent.js
+	Create and manipulate agent for abm-odm
+	
+	Sparisoma Viridi | https://github.com/dudung/abm-x
+	
+	20200604
+	0552 Start this library.
+	0740 Test random move but not painted.
+	0742 Fix color from object type.
+*/
+
+
+// Define directions
+var agentDirection = [
+	[0, 0],   // 0 Still
+	[1, 0],   // 1 Right
+	[0, -1],  // 2 Top 
+	[-1, 0],  // 3 Left
+	[0, 1],   // 4 Down
+];
+
+
+// Define Agent class
+class Agent {
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		if(arguments.length == 2) {
+			this.x = arguments[0];
+			this.y = arguments[1];
+		}
+	}
+	
+	setWorld() {
+		var w = arguments[0];
+		this.world = w;
+	}
+	
+	setType() {
+		var t = arguments[0];
+		this.type = t;
+	}
+	
+	paint() {
+		var m = this.world.m;
+		this.previousType = m[this.y][this.x];
+		m[this.y][this.x] = this.type;
+	}
+	
+	moveRandom() {
+		var xsrc = this.x;
+		var ysrc = this.y;
+		
+		var iDir = Math.floor(Math.random() * 5);
+		var dx = agentDirection[iDir][0];
+		var dy = agentDirection[iDir][1];
+		
+		var xdest = xsrc + dx;
+		var ydest = ysrc + dy;
+		
+		var m = this.world.m;
+		var type = m[ydest][xdest];
+		if(0 < type && type < 12) {
+			
+			var previousType = this.previousType; 
+			this.previousType = m[ydest][xdest];
+			
+			m[ydest][xdest] = this.type;
+			m[ysrc][xsrc] = previousType;
+			
+			this.x = xdest;
+			this.y = ydest;
+		}
+		
+	}
+}
+
