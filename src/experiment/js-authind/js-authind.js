09c4f974f5045afe233392958fa259a353445f31
dudung <dudung@gmail.com>
2020-06-11 03:56:17 +0700

    beg exp authind

---
+++ src/experiment/js-authind/js-authind.js
@@ -0,0 +1,23 @@
+/*
+	js-authind.js
+	Explore author index of something
+	
+	Sparisoma Virid | https://github.com/abm-x
+	
+	20200611
+	0355 Create this example.
+	
+	References
+	1. url .
+*/
+
+// Declare global variables
+
+
+// Execute main function
+main();
+
+
+// Define main function
+function main() {
+}
