df9d890312a329c9b3c23069098be3bd3ec67338
dudung <dudung@gmail.com>
2020-06-24 19:29:24 +0700

    beg exp of anim-slide

---
+++ src/experiment/js-anim-slide/js-anim-slide.js
@@ -0,0 +1,26 @@
+/*
+	js-anim-slide.js
+	Show animated slide with JS in a browser
+	
+	Sparisoma Viridi | https://github.com/dudung/abm-x
+	
+	20200624
+	1903 Start this experiment.
+	1928 Confuse why it does not work.
+*/
+
+// Define global variables
+var proc, iter, iterMax;
+
+
+// Execute main function
+main();
+
+
+// Define main function
+function main() {
+	var div = document.createElement("div");
+	div.innerHTML = "test";
+	document.body.append(div);
+}
+
